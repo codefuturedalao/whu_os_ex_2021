7fbb3e686a3acf80f3b42d9a6c45c9992b98059b
jacksonsang <985438046@qq.com>
2021-09-12 09:40:16 +0800

    add requirements for build in readthedocs

--- source/conf.py
+++ source/conf.py
@@ -31,8 +31,7 @@ release = 'v1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-	'recommonmark',
-    	'sphinx_markdown_tables'
+    'myst_parser'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
